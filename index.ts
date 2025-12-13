672e42865b6bc4fb3b20e3ce11167c36cd52b6f1
david-why <david_why@outlook.com>
2025-12-13 16:30:45 +0800

    create basic app structure

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { App } from '@slack/bolt'
+
+const { SLACK_BOT_TOKEN, SLACK_APP_TOKEN, SLACK_SIGNING_SECRET } = process.env
+
+const app = new App({
+  token: SLACK_BOT_TOKEN,
+  appToken: SLACK_APP_TOKEN,
+  signingSecret: SLACK_SIGNING_SECRET,
+})
+
+await app.start()
+
+console.log(':3 started!')
